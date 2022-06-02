92be370099e8d67b72fee73fcfe012f4a035ab17
hpord31 <hannu.portillo.d@uni.pe>
2022-06-02 10:41:49 -0500

    Test Premium

--- Fase4/Pruebas/AirportTest.java
+++ Fase4/Pruebas/AirportTest.java
@@ -107,5 +107,52 @@ public class AirportTest {
         }
     }
 
+    @DisplayName("Dado que hay un vuelo premium")
+    @Nested
+    class PremiumFlightTest {
+        private Flight premiumFlight;
+        private Passenger jessica;
+        private Passenger cesar;
+
+        @BeforeEach
+        void setUp() {
+            premiumFlight = new BusinessFlight("2");
+            jessica = new Passenger("Jessica", false);
+            cesar = new Passenger("Cesar", true);
+        }
+
+        @Nested
+        @DisplayName("Cuando tenemos un pasajero regular")
+        class RegularPassenger {
+
+            @Test
+            @DisplayName("Entonces no puede agregarlo o eliminarlo de un vuelo de negocios")
+            public void testBusinessFlightRegularPassenger() {
+                assertAll("Verifica todas las condiciones para un pasajero regular y un vuelo de negocios",
+                        () -> assertEquals(false, premiumFlight.addPassenger(jessica)),
+                        () -> assertEquals(0, premiumFlight.getPassengersList().size()),
+                        () -> assertEquals(false, premiumFlight.removePassenger(jessica)),
+                        () -> assertEquals(0, premiumFlight.getPassengersList().size())
+                );
+            }
+        }
+
+        @Nested
+        @DisplayName("Cuando tenemos un pasajero VIP")
+        class VipPassenger {
+
+            @Test
+            @DisplayName("Luego puedes agregarlo pero no puedes eliminarlo de un vuelo de negocios")
+            public void testBusinessFlightVipPassenger() {
+                assertAll("Verifica todas las condiciones para un pasajero VIP y un vuelo de negocios",
+                        () -> assertEquals(true, premiumFlight.addPassenger(cesar)),
+                        () -> assertEquals(1, premiumFlight.getPassengersList().size()),
+                        () -> assertEquals(false, premiumFlight.removePassenger(cesar)),
+                        () -> assertEquals(1, premiumFlight.getPassengersList().size())
+                );
+            }
+        }
+    }
+    }
 // Completa la prueba para PremiumFlight de acuerdo a la logica comercial dada. Pregunta 6
 
